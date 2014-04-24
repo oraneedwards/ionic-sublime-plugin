682b465dc001a9b1fef51dbe5bda95285dc09e02
Orane Edwards <orane.dev@gmail.com>
2014-04-24 00:55:13 -0500

    Added button v1.

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 Ionic - Sublime Plugin
 ======================
 
+[Work in Progress]
 Ionic Snippets Plugin for Sublime Text 2 &amp; 3
 
 ## Prerequisite 
 
-Technically none... but you might want to set up an [ionic project](http://ionicframework.com/getting-started/) first.
+Technically none...but you might want to have an [ionic project](http://ionicframework.com/getting-started/) setup first.
 
 ## Installing
 
@@ -13,11 +14,11 @@ Technically none... but you might want to set up an [ionic project](http://ionic
 
 Clone this repo into your sublime packages folder.
 
-    https://github.com/oraneedwards/ionic-sublime-plugin.git
+    git clone git@github.com:oraneedwards/ionic-sublime-plugin.git
 
 
 #### Manually
 
-* Download the .zip file
-* Unzip into your sublime [packages folder](http://google.com)
-* Restart Sublime if necessary
+* Download the [.zip](https://github.com/oraneedwards/ionic-sublime-plugin/archive/master.zip) file
+* Unzip into your sublime packages folder( Preferences> Browse Packages).
+* Restart Sublime if necessary.
--- generator.py
+++ generator.py
@@ -1,26 +1,73 @@
+#snippet generator
+
 import os
+
 color_opts = ['light', 'stable', 'positive', 'calm', 'balanced', 'energized', 'assertive', 'royal', 'dark']
 css_components = ['header', 'footer', 'button', 'list', 'card', 'form', 'toggle', 'checkbox', 'radio', 'range', 'select', 'tab', 'grid', 'utility']
 bars = ['header', 'footer']
-
+button_types = ['button', 'button-block', 'button-full']
 bar_template = '<snippet>\n\
 \t<content><![CDATA[\n\
-<${1:div} class="bar bar-%s bar-%s">${2:<h1 class="title">Header</h1>}</${1:div}>\n\
+<div class="bar bar-%s bar-%s">${1:<h1 class="title">Header</h1>}</div>\n\
 ]]></content>\n\
 \t<tabTrigger>ion-%s:%s</tabTrigger>\n\
+\t<source></source>\n\
+\t<description></description>\n\
 </snippet>'
+
+button_template = '<snippet>\n\
+\t<content><![CDATA[\n\
+<button class="button%s button-%s">${1:Button}</button>\n\
+]]></content>\n\
+\t<tabTrigger>ion-%s:%s</tabTrigger>\n\
+\t<source></source>\n\
+\t<description></description>\n\
+</snippet>'
+
+button_template_1 = '<snippet>\n\
+\t<content><![CDATA[\n\
+<button class="button %s">${1:Button}</button>\n\
+]]></content>\n\
+\t<tabTrigger>ion-%s:%s</tabTrigger>\n\
+\t<source></source>\n\
+\t<description></description>\n\
+</snippet>'
+
+# template ='<snippet>\n\
+# \t<content><![CDATA[\n\
+# %s\n\
+# ]]></content>\n\
+# \t<tabTrigger>ion-%s:%s</tabTrigger>\n\
+# \t<source></source>\n\
+# \t<description>Snippet for %s %s</description>\n\
+# </snippet>'
+# button_template = '<button class="button%s">${1:Button}</button>'
 
 
 for component in bars:
 
   if not os.path.exists(component):
       os.makedirs(component)
-    
+
   for color in color_opts:
     # print color
-    fname = "%s/ion-header-%s.sublime-snippet" %(component,color)
-    fo = open(fname, "w")
+    fname = '%s/ion-%s-%s.sublime-snippet' %(component,component,color)
+    fo = open(fname, 'w')
     fo.write(bar_template %(component,color,component,color))
     fo.close()
-    print fname+ " generated."
+    print fname+ ' generated.'
+
+for component in button_types:
+
+  if not os.path.exists(component):
+      os.makedirs(component)
+
+  for color in color_opts:
+    fname = '%s/ion-%s-%s.sublime-snippet' %(component,component,color)
+    fo  = open(fname, 'w')
+    fo.write(button_template %([' '+component,''][component == 'button'], color, component, color))
+    fo.close()
+    print fname + ' generated.'
+
+#TODO: add generator for README.md - Possibly add pictures
 
